497a184c77745e0bf1d7aa2ccc1b435576e31781
snail <snail_dev@163.com>
2025-07-14 20:52:57 +0800

    使用mixins语法重构相关组件less代码，并抽取vscode代码片段

--- packages/snail.view/src/components/animation.ts
+++ packages/snail.view/src/components/animation.ts
@@ -6,10 +6,10 @@
  * 注意事项：
  *  1、不提供全局【观察者】对象；这个涉到不少子scope的销毁，在全局挂着始终不好
  */
-import { IScope, IScopes, isObject, mountScope, throwIfFalse, useScopes } from "snail.core";
-import { TransitionEffectOptions, IAnimationManager } from "../models/animation-model";
+import { IScope, IScopes, isObject, ITimer, mountScope, throwIfFalse, useScopes, useTimer } from "snail.core";
+import { TransitionEffect, IAnimationManager } from "../models/animation-model";
 import { getAnimationScope } from "../utils/animation-util";
-import { css } from "./css";
+import { CSS, css } from "./css";
 
 // 把自己的类型共享出去
 export * from "../models/animation-model";
@@ -26,10 +26,10 @@ export function useAnimation(): IAnimationManager & IScope {
     /**
      * 过渡动画：从初始样式变为目标样式
      * - 执行顺序 from - to - end
-     * - 开始动画时，清理 to end 操作样式，设置 from 再设异步置 to 样式
+     * - 开始动画时，清理 to end 操作样式，设置 from 再设异步设置 to 样式
      * - 动画结束后，清理 from to 操作样式，保留 end 样式
      * @param el 执行动画的元素
-     * @param effect 动画效果配置，约束 from to end 样式
+     * @param effect 过渡效果配置，约束 from to end 样式
      * @param time 动画持续时长，单位ms，默认200ms；动画结束后销毁作用域
      * @returns 动画作用域
      * @example 将div的高度在1s内从100px变为200px。动画结束后保持200px高度，则可以使用如下代码：
@@ -39,23 +39,25 @@ export function useAnimation(): IAnimationManager & IScope {
      *      end: { height: "200px" }
      * }, 1000);
      */
-    function transition(el: HTMLElement, effect: TransitionEffectOptions, time?: number): IScope {
+    function transition(el: HTMLElement, effect: TransitionEffect<CSS>, time?: number): IScope {
         throwIfFalse(isObject(effect), "transition: effect must be an object.");
         const scope = scopes.add(getAnimationScope(manager, el));
         const fromCss = css.parse(effect.from);
         const toCss = css.parse(effect.to);
         const endCss = css.parse(effect.end);
-        //  启动动画：先 清理 to end ；设置 from ，然后异步设置 to
+        //  启动动画：先 清理 to end ；设置 from ，然后异步设置 to，并延迟销毁作用域
         css.operate(el, "clear", toCss);
         css.operate(el, "clear", endCss);
         css.operate(el, "add", fromCss);
-        setTimeout(css.operate, 1, el, "add", toCss);
-        //  返回动画作用域：scope销毁时：执行样式清理；动画结束后，强制销毁scope
-        setTimeout(scope.destroy, time > 0 ? time : 200);
+        const toId = setTimeout(css.operate, 1, el, "add", toCss);
+        const endId = setTimeout(scope.destroy, time > 0 ? time : 200);
+        //  作用域销毁时：清理from、to样式，设置end样式，并清理用到的定时器
         return scope.onDestroy(function () {
             css.operate(el, "clear", fromCss);
             css.operate(el, "clear", toCss);
             css.operate(el, "add", endCss);
+            clearTimeout(toId);
+            clearTimeout(endId);
         });
     }
     //#endregion
--- packages/snail.view/src/models/animation-model.ts
+++ packages/snail.view/src/models/animation-model.ts
@@ -11,7 +11,7 @@ export interface IAnimationManager {
      * - 开始动画时，清理 to end 操作样式，设置 from 再设异步置 to 样式
      * - 动画结束后，清理 from to 操作样式，保留 end 样式
      * @param el 执行动画的元素
-     * @param effect 动画效果配置，约束 from to end 样式
+     * @param effect 过渡效果配置，约束 from to end 样式
      * @param time 动画持续时长，单位ms，默认200ms；动画结束后销毁作用域
      * @returns 动画作用域
      * @example 将div的高度在1s内从100px变为200px。动画结束后保持200px高度，则可以使用如下代码：
@@ -21,29 +21,30 @@ export interface IAnimationManager {
      *      end: { height: "200px" }
      * }, 1000);
      */
-    transition(el: HTMLElement, effect: TransitionEffectOptions, time?: number): IScope;
+    transition(el: HTMLElement, effect: TransitionEffect<CSS>, time?: number): IScope;
 }
 
 /**
  * 过渡动画效果配置信息
  * - 执行顺序 from - to - end
+ * - 支持泛型，扩展更通用的过渡表述场景
  */
-export type TransitionEffectOptions = {
+export type TransitionEffect<T> = {
     /**
      * 过渡动画开始样式
      * - 支持 class 、style 样式属性
      * - 如 高度 从 100 到 200；100 则为动画开始样式
      */
-    from: CSS;
+    from: T;
     /**
      * 过渡动画目标样式
      * - 支持 class 、style 样式属性
      * - 如 高度 从 100 到 200；200 则为动画目标样式
      */
-    to: CSS;
+    to: T;
     /**
      * 动画结束样式
      * - 支持 class 、style 样式属性
      */
-    end?: CSS;
+    end?: T;
 }
