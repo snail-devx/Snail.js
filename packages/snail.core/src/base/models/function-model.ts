5f7dde171ce9636319e3e9da43a8d9db1c3c2005
snail <snail_dev@163.com>
2026-02-04 21:40:17 +0800

    RunResult支持无泛型参数，相关包同步.core包版本

--- packages/snail.core/src/base/models/function-model.ts
+++ packages/snail.core/src/base/models/function-model.ts
@@ -1,24 +1,23 @@
 /**
  * 运行结果
  */
-export type RunResult<T> = {
+export type RunResult<T = void> = {
     /**
      * 是否成功
      */
-    success: boolean,
-    /**
-     * 返回数据
-     */
-    data?: T,
+    success: boolean;
 
     /**
-     * 运行失败时的异常
+     * 运行结果数据
+     */
+    data?: T;
+
+    /**
+     * 运行时发生的异常
      */
     ex?: Error;
     /**
      * 运行失败的原因
-     * - 运行发生异常时则为 ex.message 
-     * - 否则为失败原因自身对象
      */
     reason?: any;
 }
