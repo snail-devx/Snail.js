5b917178595278b533b3f921280c2d4ddac06a43
snail <snail_dev@163.com>
2026-02-22 18:35:14 +0800

    完善Transitions组件，内置动画实现

--- packages/snail.vue/src/container/models/transition-model.ts
+++ packages/snail.vue/src/container/models/transition-model.ts
@@ -3,14 +3,41 @@
  */
 export type TransitionOptions = {
     /**
-     * 动画效果样式
-     * - 填写自定义的效果样式class名；不会作为 Transition的 name 属性名
-     * - 默认为 snail-transition
-     * - 动画的各个阶段，在此样式下增加 
-     * - - enter-active enter-from enter-to
-     * - - leave-active leave-from leave-to
+     * 动画的自定义类样式名称
+     * - 默认为 `snail-transition`；不会作为 Transition的 name 属性名
+     * - 动画生效时组成的`class`规则为：`customClass`+动画状态；类似`snail-transition enter-from`
+     * - 动画状态可选值：
+     *  |-------------------|----------------------|
+     *  |   --              |--                    |
+     *  |   enter-active    | 进入动画的生效状态    |
+     *  |   enter-from      | 进入动画的起始状态    |
+     *  |   enter-to        | 进入动画的结束状态    |
+     *  |   leave-active    | 离开动画的生效状态    |
+     *  |   leave-from      | 离开动画的起始状态    |
+     *  |   leave-to        | 离开动画的结束状态    |
+     * 
      */
-    effect?: string;
+    customClass?: string;
+    /**
+     * 动画效果
+     * - 配合 `customClass` 使用，用于标记动画的具体实现
+     * - 作为默认动画效果，组装出来的动画效果类似 `snail-transition fade enter-from`
+     * - 效果可选值；默认值 fade
+     *  |----------------|-------------|----------------------------------|---------------------------------|
+     *  |   --           |--           |--                                |--                               |
+     *  |   `fade`       | 淡入淡出     | 进入时 `opacity` 0-1             | 离开时 `opacity` 1 - 0           |
+     *  |   `scale`      | 缩放动画     | 进入时 `scale` 0-1               | 离开时 `scale` 0 - 1             |
+     *  |   `rotate`     | 旋转动画     | 进入时 `rotate` 360deg - 0       | 离开时 `rotate` 0 - 360deg       |
+     *  |   `up`         | 上进上出     | 进入时 `translateY` 100% - 0     | 离开时 `translateY` 0 - -100%    |
+     *  |   `down`       | 下进下出     | 进入时 `translateY` -100% - 0    | 离开时 `translateY` 0 - 100%     |
+     *  |   `up-down`    | 上进下出     | 进入时 `translateY` 100% - 0     | 离开时 `translateY` 0 - 100%     |
+     *  |   `down-up`    | 下进上出     | 进入时 `translateY` -100% - 0    | 离开时 `translateY` 0 - -100%    |
+     *  |   `left`       | 左进左出     | 进入时 `translateX` -100% - 0    | 离开时 `translateX` 0 - -100%    |
+     *  |   `right`      | 右进右出     | 进入时 `translateX` 100% - 0     | 离开时 `translateX` 0 - 100%     |
+     *  |   `left-right` | 左进右出     | 进入时 `translateX` -100% - 0    | 离开时 `translateX` 0 - 100%     |
+     *  |   `right-left` | 右进左出     | 进入时 `translateX` 100% - 0     | 离开时 `translateX` 0 - -100%    |
+     */
+    effect?: TransitionEffect | Array<TransitionEffect>;
     /**
      * 是分组动画，还是单个动画
      * - true 则使用  TransitionGroup
@@ -18,3 +45,6 @@ export type TransitionOptions = {
      */
     group: boolean;
 }
+export type TransitionEffect = "fade" | "scale" | "rotate"
+    | "up" | "down" | "up-down" | "down-up"
+    | "left" | "right" | "left-right" | "right-left";
--- packages/snail.vue/src/container/transitions.vue
+++ packages/snail.vue/src/container/transitions.vue
@@ -2,7 +2,7 @@
         1、利用group属性，同时实现 Transition、TransitionGroup
         2、使用自定义的类样式+阶段类样式名，从而实现效果范围框定
             不使用 类似“*-enter-from”，太累赘且没有结构从属，容易和全局冲突
-        3、后续支持在 effect 基础上，在加上 修饰，从而实现一些内置动画效果，如 fade 、scale 实现探入淡出、缩放等
+        3、后续参照 https://animate.style/ 支持更多动画样式
         4、外部需要自定义动画效果时，采用如下结构；解构更加清晰
             .snail-transition {
 
@@ -26,10 +26,10 @@
             }
   -->
 <template>
-    <component :is="group ? TransitionGroup : Transition" appear :enter-active-class="`${effect} enter-active`"
-        :enter-from-class="`${effect} enter-from`" :enter-to-class="`${effect} enter-to`"
-        :leave-active-class="`${effect} leave-active`" :leave-from-class="`${effect} leave-from`"
-        :leave-to-class="`${effect} leave-to`">
+    <component :is="group ? TransitionGroup : Transition" appear :enter-active-class="`${classNames} enter-active`"
+        :enter-from-class="`${classNames} enter-from`" :enter-to-class="`${classNames} enter-to`"
+        :leave-active-class="`${classNames} leave-active`" :leave-from-class="`${classNames} leave-from`"
+        :leave-to-class="`${classNames} leave-to`">
         <slot />
     </component>
 </template>
@@ -37,11 +37,25 @@
 <script setup lang="ts">
 import { computed, ref, shallowRef, Transition, TransitionGroup } from "vue";
 import { TransitionOptions } from "./models/transition-model";
+import { isArray } from "snail.core";
 
 // *****************************************   👉  组件定义    *****************************************
-//  1、props、event、model、components
-const { effect = "snail-transition" } = defineProps<TransitionOptions>();
+//  1、props、event、model、components "snail-transition"
+const props = defineProps<TransitionOptions>();
 //  2、组件交互变量、常量
+/**     动画的类样式名数值，“ ”连接 */
+const classNames = computed<string>(() => {
+    const array = [props.customClass || "snail-transition"];
+    if (isArray(props.effect)) {
+        props.effect.length > 0
+            ? array.push(...props.effect)
+            : array.push("fade");
+    }
+    else {
+        array.push(props.effect as string || "fade")
+    }
+    return array.join(" ");
+});
 
 // *****************************************   👉  方法+事件    ****************************************
 
@@ -59,20 +73,175 @@ const { effect = "snail-transition" } = defineProps<TransitionOptions>();
 
     &.enter-active,
     &.leave-active {
-        overflow: hidden;
-        transition: all 0.2s ease;
+        transition-property: all;
+        transition-duration: 0.4s;
+        transition-timing-function: ease-in-out;
+    }
+}
+
+//  淡入淡出     | 进入时 `opacity` 0-1             | 离开时 `opacity` 1 - 0
+.snail-transition.fade {
+
+    &.enter-active,
+    &.leave-active {
+        opacity: 1;
+    }
+
+    &.enter-from,
+    &.leave-to {
+        opacity: 0;
+    }
+}
+
+//  缩放动画     | 进入时 `scale` 0-1               | 离开时 `scale` 0 - 1            
+.snail-transition.scale {
+
+    &.enter-active,
+    &.leave-active {
+        transform: scale(0);
+    }
+
+    &.enter-to,
+    &.leave-from {
+        transform: scale(1);
+    }
+}
+
+//  旋转动画     | 进入时 `rotate` 360deg - 0       | 离开时 `rotate` 0 - 360deg 
+.snail-transition.rotate {
+
+    &.enter-active,
+    &.leave-active {
+        transform: rotate(360deg);
+    }
+
+    &.enter-to,
+    &.leave-from {
+        transform: rotate(0);
+    }
+}
+
+//  上进上出     | 进入时 `translateY` 100% - 0     | 离开时 `translateY` 0 - -100% 
+.snail-transition.up {
+
+    &.enter-active,
+    &.leave-active {
+        transform: translateY(0);
     }
 
     &.enter-from {
-        opacity: 0;
-        transform: translateY(-30px);
+        transform: translateY(100%);
     }
 
     &.leave-to {
-        opacity: 0;
-        // transform: translateX(30px);
-        transform: translateY(30px);
-        // scale: 0.4;
+        transform: translateY(-100%);
+    }
+}
+
+//  下进下出     | 进入时 `translateY` -100% - 0    | 离开时 `translateY` 0 - 100% 
+.snail-transition.down {
+
+    &.enter-active,
+    &.leave-active {
+        transform: translateY(0);
+    }
+
+    &.enter-from {
+        transform: translateY(-100%);
+    }
+
+    &.leave-to {
+        transform: translateY(100%);
+    }
+}
+
+//  上进下出     | 进入时 `translateY` 100% - 0     | 离开时 `translateY` 0 - 100%
+.snail-transition.up-down {
+
+    &.enter-active,
+    &.leave-active {
+        transform: translateY(0);
+    }
+
+    &.enter-from,
+    &.leave-to {
+        transform: translateY(100%);
+    }
+}
+
+//  下进上出     | 进入时 `translateY` -100% - 0    | 离开时 `translateY` 0 - -100%
+.snail-transition.down-up {
+
+    &.enter-active,
+    &.leave-active {
+        transform: translateY(0);
+    }
+
+    &.enter-from,
+    &.leave-to {
+        transform: translateY(-100%);
+    }
+}
+
+//  左进左出     | 进入时 `translateX` -100% - 0    | 离开时 `translateX` 0 - -100%
+.snail-transition.left {
+
+    &.enter-active,
+    &.leave-active {
+        transform: translateX(0);
+    }
+
+    &.enter-from,
+    &.leave-to {
+        transform: translateX(-100%);
+    }
+}
+
+//  右进右出     | 进入时 `translateX` 100% - 0     | 离开时 `translateX` 0 - 100%
+.snail-transition.right {
+
+    &.enter-active,
+    &.leave-active {
+        transform: translateX(0);
+    }
+
+    &.enter-from,
+    &.leave-to {
+        transform: translateX(100%);
+    }
+}
+
+//  左进右出     | 进入时 `translateX` -100% - 0    | 离开时 `translateX` 0 - 100%
+.snail-transition.left-right {
+
+    &.enter-active,
+    &.leave-active {
+        transform: translateX(0);
+    }
+
+    &.enter-from {
+        transform: translateX(-100%);
+    }
+
+    &.leave-to {
+        transform: translateX(100%);
+    }
+}
+
+//  右进左出     | 进入时 `translateX` 100% - 0     | 离开时 `translateX` 0 - -100% 
+.snail-transition.right-left {
+
+    &.enter-active,
+    &.leave-active {
+        transform: translateX(0);
+    }
+
+    &.enter-from {
+        transform: translateX(100%);
+    }
+
+    &.leave-to {
+        transform: translateX(-100%);
     }
 }
 </style>
