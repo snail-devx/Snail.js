428d2e44b7eb309614ce2b7f6fea773ed75d8d73
snail <snail_dev@163.com>
2026-03-10 20:12:35 +0800

    1、TimePicker组件 时间值相关代码抽取core后的适配 2、TimePicker组件 界面调整，简化仅显示时分秒和操作按钮

--- packages/snail.vue/src/picker/manager.ts
+++ packages/snail.vue/src/picker/manager.ts
@@ -1,10 +1,9 @@
-import { IAsyncScope, IScope, mountScope, useScopes } from "snail.core";
+import { IAsyncScope, IScope, mountScope, newId, useScopes } from "snail.core";
 import { IPickerManager } from "./models/picker-model";
 import { TimePickerOptions } from "./models/datetime-model";
 import { usePopup } from "../popup/manager";
 
 import TimePc from "./components/time-pc.vue";
-
 
 /**
  * 使用选择器
@@ -21,17 +20,20 @@ export function usePicker(): IPickerManager & IScope {
      * @param options 时间选择控件配置选项
      * @returns 异步任务，可销毁时间选择控件；可接收时间选择控件的选择值
      */
-    function showTime(target: HTMLElement, options?: TimePickerOptions): IAsyncScope<string> {
+    function showTime(target: HTMLElement, options?: Omit<TimePickerOptions, "manager">): IAsyncScope<string> {
+        //  这里不用外部传入 manager 属性
+        options = { ...options };
+        delete options["manager"];
         //  判断弹出PC还是移动端选择
         return popup.follow<string, TimePickerOptions>(target, {
             component: TimePc,
-            followX: ["center", "ratio"],
+            followX: ["center"],
             spaceClient: 10,
-            spaceY: 4,
-            spaceX: 10,
+            spaceY: 2,
+            spaceX: 2,
             closeOnMask: true,
             //  时间选择器属性
-            props: options || Object.create(null),
+            props: options,
         });
     }
 
