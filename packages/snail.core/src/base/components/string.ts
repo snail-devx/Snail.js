a833633b83d883e6c1395daa34af6e3631070497
snail <snail_dev@163.com>
2026-06-01 21:12:53 +0800

    修复correctString未使用trim参数的问题，并将tidyString标记为已废弃

--- packages/snail.core/src/base/components/string.ts
+++ packages/snail.core/src/base/components/string.ts
@@ -44,12 +44,19 @@ export function mustString(data: any, paramName: string): boolean {
  * @returns 校正后的字符串
  */
 export function correctString(str: any, newValue: string, trim: boolean): string {
-    return isStringNotEmpty(str) ? str : newValue;
+    if (isString(str) == true) {
+        trim == true && (str = (str as string).trim());
+        return (str as string).length
+            ? str
+            : newValue;
+    }
+    return newValue;
 }
 /**
  * 整理字符串；去除前后空格，空或者非字符串强制undeined
  * @param str 要处理的字符串数据
  * @returns 去除前后空格的字符串
+ * @deprecated 已废弃，请使用 @see correctString
  */
 export function tidyString(str: any): string | undefined {
     //  里面内置了trim逻辑，先保持现状；新的推荐 correctString
